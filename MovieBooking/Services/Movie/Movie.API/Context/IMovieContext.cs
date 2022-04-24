bb14f53d211619176913d3bcf5b09375f5aa11d6
Немања <nemanja.zivanovic97@hotmail.com>
2022-04-24 09:34:46 +0200

    feat Implemented context for movies

---
+++ MovieBooking/Services/Movie/Movie.API/Context/IMovieContext.cs
@@ -0,0 +1,14 @@
+﻿using MongoDB.Driver;
+using Movies.API.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Movies.API.Context
+{
+    public interface IMovieContext
+    {
+        IMongoCollection<MovieDTO> Movies { get; }
+    }
+}
---
+++ MovieBooking/Services/Movie/Movie.API/Context/MovieContext.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+using Movies.API.DTOs;
+using Movies.API.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Movies.API.Context
+{
+    public class MovieContext : IMovieContext
+    {
+        public MovieContext(IConfiguration configuration, IMapper mapper)
+        {
+            var client = new MongoClient(configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+            var database = client.GetDatabase("MoviesDB");
+            Movies = mapper.Map<IMongoCollection<MovieDTO>>(database.GetCollection<Movie>("Movies"));
+        }
+
+        public IMongoCollection<MovieDTO> Movies { get; }
+    }
+}
