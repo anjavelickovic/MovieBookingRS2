c01f1ef16b9715f15d510f40fccf32349786e7b3
milicas19 <milica7amg@gmail.com>
2022-04-24 14:26:28 +0200

    Closes #1; Added entities for Reservations.API

---
+++ MovieBooking/Services/Reservations/Reservations.API/Entities/Reservation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Reservations.API.Entities
+{
+    public class Reservation
+    { 
+        public int MovieId { get; set; } 
+        public string MovieName { get; set; }
+        public int TheaterHallId { get; set; }
+        public string TheaterHallName { get; set; }
+        public DateTime DateAndTimeOfTheMovie { get; set; }
+        public decimal Price { get; set; }
+        public int NumberOfTickets { get; set; }
+    }
+}
---
+++ MovieBooking/Services/Reservations/Reservations.API/Entities/Reservations.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Reservations.API.Entities{
+
+    public class Reservations{
+
+        public string Username { get; set; }
+        public List<Reservation> ListOfReservations { get; set; } = new List<Reservation>();
+
+        public Reservations(){}
+        public Reservations(string username) {
+            Username = username ?? throw new ArgumentNullException(nameof(username));
+        }
+
+        public decimal TotalPrice {
+            get{
+                decimal totalPrice = 0;
+                foreach (var reservation in ListOfReservations)
+                {
+                    totalPrice += reservation.Price * reservation.NumberOfTickets;
+                }
+                return totalPrice;
+            }
+        }
+    }
+}
