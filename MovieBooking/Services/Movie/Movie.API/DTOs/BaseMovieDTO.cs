bdcb40a78805cbaa655b882c51fc42055cc5a031
Немања <nemanja.zivanovic97@hotmail.com>
2022-04-24 08:49:32 +0200

    feat Added DTOs for movie entity

---
+++ MovieBooking/Services/Movie/Movie.API/DTOs/BaseMovieDTO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Movie.API.DTOs
+{
+    public class BaseMovieDTO
+    {
+        public string Id { get; set; }
+        public int Year { get; set; }
+        public string Released { get; set; }
+        public string Runtime { get; set; }
+        public string[] Genres { get; set; }
+        public string Director { get; set; }
+        public string[] MainActors { get; set; }
+        public string Plot { get; set; }
+        public string Language { get; set; }
+        public string Poster { get; set; }
+        public decimal ImdbRating { get; set; }
+        public int ImdbVotes { get; set; }
+        public string Trailer { get; set; }
+    }
+}
