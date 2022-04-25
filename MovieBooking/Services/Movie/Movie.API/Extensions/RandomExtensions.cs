894c0b5932c6a3fe89e8ddeb1c14e261d302461e
Немања <nemanja.zivanovic97@hotmail.com>
2022-04-25 14:49:43 +0200

    feat Added extension method for random class
    Method Sample returns numberOfElements unique integers less than maxValue.

---
+++ MovieBooking/Services/Movie/Movie.API/Extensions/RandomExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Movies.API.Extensions
+{
+    public static class RandomExtensions
+    {
+        public static IEnumerable<int> Sample(this Random random, int maxValue, int numberOfElements)
+        {
+            var numbers = new HashSet<int>();
+            while (numbers.Count < numberOfElements)
+            {
+                numbers.Add(random.Next(maxValue));
+            }
+            return numbers.ToList();
+        }
+    }
+}
