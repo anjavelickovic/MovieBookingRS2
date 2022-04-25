aa250dd0fd4fdcadb08f7485e17e56b40795b3fd
anjavelickovic <28anja@live.com>
2022-04-25 18:41:17 +0200

    Created class Coupon

---
+++ MovieBooking/Services/Discount/Discount.Common/Discount.Common/Entities/Coupon.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Discount.Common.Entities
+{
+    public class Coupon
+    {
+        public string Id { get; set; }
+        public string MovieName { get; set; }
+        public int Amount { get; set; }
+    }
+}
