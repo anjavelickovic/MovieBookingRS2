2c137c5a5ae22988999172ee9d3defa047676af6
anjavelickovic <28anja@live.com>
2022-04-26 17:40:25 +0200

    Created Discount.API

---
+++ MovieBooking/Services/Discount/Discount.API/Discount.API/Discount.API/Controllers/DiscountController.cs
@@ -0,0 +1,22 @@
+﻿using Discount.Common.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Discount.API.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class DiscountController : ControllerBase
+    {
+        private readonly ICouponRepository _repository;
+
+        public DiscountController(ICouponRepository repository)
+        {
+            _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+        }
+
+    }
+}
