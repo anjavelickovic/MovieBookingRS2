45ddd081c5bcf652296b82bad260271f5327dcb8
Немања <nemanja.zivanovic97@hotmail.com>
2022-04-27 18:18:34 +0200

    fix BaseMovieDTO update changes in movie entity

--- MovieBooking/Services/Movie/Movie.API/DTOs/BaseMovieDTO.cs
+++ MovieBooking/Services/Movie/Movie.API/DTOs/BaseMovieDTO.cs
@@ -8,6 +8,7 @@ namespace Movies.API.DTOs
     public class BaseMovieDTO
     {
         public string Id { get; set; }
+        public string Title { get; set; }
         public int Year { get; set; }
         public string Released { get; set; }
         public string Runtime { get; set; }
@@ -15,7 +16,7 @@ namespace Movies.API.DTOs
         public string Director { get; set; }
         public string[] MainActors { get; set; }
         public string Plot { get; set; }
-        public string Language { get; set; }
+        public string[] Languages { get; set; }
         public string Poster { get; set; }
         public decimal ImdbRating { get; set; }
         public int ImdbVotes { get; set; }
