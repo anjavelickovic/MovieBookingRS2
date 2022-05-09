12377486238c8d9bd4c79715758e558a7d761d1f
milicas19 <milica7amg@gmail.com>
2022-05-09 16:42:42 +0200

    Closes #31; Added entities for Projection.Common

---
+++ MovieBooking/Services/Projection/Projections.Common/Entities/Projections.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Projection.Common.Entities
+{
+    public class Projections
+    {
+        public string MovieId { get; set; }
+        public string MovieTitle { get; set; }
+        public string Runtime { get; set; }
+        public string TheaterHallId { get; set; }
+        public string TheaterHallName { get; set; }
+        public int NumberOfSeats { get; set; }
+        public DateTime ProjectionStartTime { get; set; }
+        public DateTime ProjectionFinishTime
+        {
+            get
+            {
+                var RuntimeInt = Int32.Parse(Runtime.Substring(0, Runtime.Length - 4));
+                DateTime finishDateTime = ProjectionStartTime.AddMinutes(RuntimeInt);
+                return finishDateTime;
+            }
+        }
+        public int NumberOfReservedSeats { get; set; }
+    }
+}
