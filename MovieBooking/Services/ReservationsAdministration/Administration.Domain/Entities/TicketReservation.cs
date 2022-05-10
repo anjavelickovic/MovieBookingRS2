f366f9f7f1d849d6e80754268c314fe5e9d4b7bb
anjavelickovic <28anja@live.com>
2022-05-10 20:49:43 +0200

    added entity

---
+++ MovieBooking/Services/ReservationsAdministration/Administration.Domain/Entities/TicketReservation.cs
@@ -0,0 +1,41 @@
+﻿using Administration.Domain.Exceptions;
+using Ordering.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Administration.Domain.Entities
+{
+    public class TicketReservation : EntityBase
+    {
+        public string MovieTitle { get; private set; }
+        public string MovieId { get; private set; }
+        public decimal Price { get; private set; }
+        public string TheaterHallId { get; set; }
+        public string TheaterHallName { get; set; }
+        public int NumberOfTickets { get; private set; } = 0;
+
+        public TicketReservation(string movieTitle, string movieId, decimal price, string theaterHallId, string theaterHallName, int numberOfTickets)
+        {
+            MovieTitle = movieTitle ?? throw new ArgumentNullException(nameof(movieTitle));
+            MovieId = movieId ?? throw new ArgumentNullException(nameof(movieId));
+            TheaterHallId = theaterHallId ?? throw new ArgumentNullException(nameof(theaterHallId));
+            TheaterHallName = theaterHallName ?? throw new ArgumentNullException(nameof(theaterHallName));
+            Price = price;
+            AddTickets(numberOfTickets);
+        }
+
+        private void AddTickets(int numberOfTickets)
+        {
+            var newNumber = numberOfTickets + NumberOfTickets;
+
+            if(newNumber <= 0) {
+                throw new AdministrationDomainException("Invalid number of tickets");   
+            }
+
+            NumberOfTickets = newNumber;
+        }
+    }
+}
