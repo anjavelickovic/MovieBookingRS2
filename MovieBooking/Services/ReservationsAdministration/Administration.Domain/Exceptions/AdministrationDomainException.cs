56116f70d11c6bfd660583660b55ff5f612d8dfa
anjavelickovic <28anja@live.com>
2022-05-10 20:50:41 +0200

    added exception

---
+++ MovieBooking/Services/ReservationsAdministration/Administration.Domain/Exceptions/AdministrationDomainException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Administration.Domain.Exceptions
+{
+    public class AdministrationDomainException : Exception
+    {
+        public AdministrationDomainException()
+        {
+        }
+
+        public AdministrationDomainException(string message) 
+            : base(message)
+        {
+        }
+
+        public AdministrationDomainException(string message, Exception innerException) 
+            : base(message, innerException)
+        {
+        }
+    }
+}
