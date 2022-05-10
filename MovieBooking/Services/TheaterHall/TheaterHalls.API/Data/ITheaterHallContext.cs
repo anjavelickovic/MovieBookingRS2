3c026e23324d9031a9689f0a07c26ec6ba2c718d
milicas19 <milica7amg@gmail.com>
2022-05-10 14:41:50 +0200

    Closes #41; Added TheaterHalls microservice

---
+++ MovieBooking/Services/TheaterHall/TheaterHalls.API/Data/ITheaterHallContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using MongoDB.Driver;
+using TheaterHalls.API.Entities;
+
+namespace TheaterHalls.API.Data
+{
+    public interface ITheaterHallContext
+    {
+        IMongoCollection<TheaterHall> TheaterHalls { get; }
+    }
+}
