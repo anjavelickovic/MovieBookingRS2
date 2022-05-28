3795c5b4f78ff3b318584c25f4d598295f2aa428
Немања <nemanja.zivanovic97@hotmail.com>
2022-05-28 09:21:29 +0200

    feat Added access token to HTTP header when requesting user details

--- MovieBooking/MovieBookingSPA/src/app/identity/domain/infrastructure/user.service.ts
+++ MovieBooking/MovieBookingSPA/src/app/identity/domain/infrastructure/user.service.ts
@@ -1,15 +1,27 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, switchMap, take } from 'rxjs';
+import { IAppState } from 'src/app/shared/app-state/app-state';
+import { AppStateService } from 'src/app/shared/app-state/app-state.service';
 import { IUserDetails } from '../models/user-details';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
-  constructor(private httpClient: HttpClient) {}
+  constructor(private httpClient: HttpClient, private appStateService: AppStateService) {}
 
   public getUserDetails(usernameOrEmail: string): Observable<IUserDetails> {
-    return this.httpClient.get<IUserDetails>(`http://localhost:4000/api/v1/User/${usernameOrEmail}`);
+    return this.appStateService.getAppState().pipe(
+      take(1),
+      switchMap((appState: IAppState) => {
+        const accessToken: string | undefined = appState.accessToken;
+
+        const headers: HttpHeaders = new HttpHeaders().append('Authorization', `Bearer ${accessToken}`);
+
+        return this.httpClient.get<IUserDetails>(`http://localhost:4000/api/v1/User/${usernameOrEmail}`,
+        { headers });
+      })
+    );
   }
 }
