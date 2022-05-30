24956f32a916a58313c7262b3422ee63896e7ac3
milicas19 <milica7amg@gmail.com>
2022-05-31 00:30:49 +0200

    Closes #49, closes #53; Added Administration.API and EventBusConsumers in it

--- MovieBooking/Common/EventBus.Messages/Constants/EventBusConstant.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace EventBus.Messages.Constants
-{
-    public static class EventBusConstant
-    {
-        public const string ReservationCheckoutQueue = "reservationcheckout-queue";
-    }
-}
---
+++ MovieBooking/Services/ReservationsAdministration/Administration.API/Mapper/ReservationProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Administration.Application.Features.Commands.CreateReservation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Administration.API.Mapper
+{
+    public class ReservationProfile : Profile
+    {
+        public ReservationProfile()
+        {
+            CreateMap<CreateReservationCommand, EventBus.Messages.Events.ReservationBasketCheckoutEvent>().ReverseMap();
+            CreateMap<Administration.Application.Features.Commands.DTOs.TicketDTO, EventBus.Messages.Events.ReservationItem>().ReverseMap();
+        }
+    }
+}
