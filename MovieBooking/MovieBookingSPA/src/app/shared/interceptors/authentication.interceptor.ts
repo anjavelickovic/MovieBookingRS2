7b44c1112b375e0ec6d9d26f0e681b6786e4edf0
Немања <nemanja.zivanovic97@hotmail.com>
2022-05-30 08:22:22 +0200

    feat Added interceptor

---
+++ MovieBooking/MovieBookingSPA/src/app/shared/interceptors/authentication.interceptor.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor,
+  HttpErrorResponse
+} from '@angular/common/http';
+import { catchError, Observable, switchMap, take, throwError } from 'rxjs';
+import { AppStateService } from '../app-state/app-state.service';
+import { IAppState } from '../app-state/app-state';
+
+@Injectable()
+export class AuthenticationInterceptor implements HttpInterceptor {
+  private readonly whitelistUrls: string[] = ['/api/v1/CustomerAuthentication/LoginCustomer',
+                                              '/api/v1/AdminAuthentication/LoginAdmin'];
+
+  constructor(private appStateService: AppStateService) {}
+
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    if (this.isWhitelisted(request.url)) {
+      return next.handle(request);
+    }
+    
+    return this.appStateService.getAppState().pipe(
+      take(1),
+      switchMap((appState: IAppState) => {
+        if (appState.accessToken !== undefined) {
+          request = this.addToken(request, appState.accessToken);
+        }
+
+        return next.handle(request);
+      }));
+ }
+
+  private isWhitelisted(url: string): boolean {
+    return this.whitelistUrls.some((whitelistedUrl: string) => url.includes(whitelistedUrl));
+  }
+
+  private addToken(request: HttpRequest<unknown>, accessToken: string): HttpRequest<unknown> {
+    return request.clone({
+      setHeaders: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+  }
+}
