715787389084560f6fc3ee43abe2bd69529e54fa
Немања <nemanja.zivanovic97@hotmail.com>
2022-08-12 04:18:54 +0200

    feat Added support for changing first and last name in Identity service

---
+++ MovieBooking/Services/Security/Identity/DTOs/ChangeFirstNameDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Identity.DTOs
+{
+    public class ChangeFirstNameDTO
+    {
+        [Required(ErrorMessage = "FirstName is required")]
+        public string NewFirstName { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        public string Password { get; set; }
+    }
+}
---
+++ MovieBooking/Services/Security/Identity/DTOs/ChangeLastNameDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Identity.DTOs
+{
+    public class ChangeLastNameDTO
+    {
+        [Required(ErrorMessage = "LastName is required")]
+        public string NewLastName { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        public string Password { get; set; }
+    }
+}
