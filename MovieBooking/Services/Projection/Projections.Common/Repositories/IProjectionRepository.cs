99cd943d3cd04622f534864e41d8f91c81f3eb0f
milicas19 <milica7amg@gmail.com>
2022-08-20 18:38:20 +0200

    Closes #109, closes #110; Configured CORS policy for microservis projection and implemented new methods in controller

--- MovieBooking/Services/Projection/Projections.Common/Repositories/IProjectionRepository.cs
+++ MovieBooking/Services/Projection/Projections.Common/Repositories/IProjectionRepository.cs
@@ -10,11 +10,13 @@ namespace Projections.Common.Repositories
         Task<IEnumerable<Projection>> GetProjections();
         Task<IEnumerable<Projection>> GetProjectionsByDate(string date);
         Task<IEnumerable<Projection>> GetMovieProjections(string movieId);
+        Task<IEnumerable<Projection>> GetMovieProjectionsByTitle(string movieId);
         Task<Projection> GetProjection(string id);
         Task CreateProjection(Projection projection);
         Task<bool> UpdateProjection(Projection projection);
         Task<bool> DeleteProjection(string id);
-        Task<bool> DeleteMovieProjections(string movieId);
+        Task<bool> DeleteProjectionsByMovieTitle(string movieId);
+        Task<bool> DeleteProjectionsByDate(string date);
         Task<bool> DeleteProjections();
     }
 }
