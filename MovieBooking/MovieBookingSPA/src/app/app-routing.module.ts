6d5146aaf90d70deb751980e2d5e308fd707c174
milicas19 <milica7amg@gmail.com>
2022-08-20 18:34:05 +0200

    Added projection module

--- MovieBooking/MovieBookingSPA/src/app/app-routing.module.ts
+++ MovieBooking/MovieBookingSPA/src/app/app-routing.module.ts
@@ -7,6 +7,8 @@ const routes: Routes = [
   { path: 'identity', loadChildren: () => import('./identity/identity.module').then(m => m.IdentityModule) },
   { path: 'theater-hall', loadChildren: () => import('./theater-hall/theater-hall.module').then(m => m.TheaterHallModule), canActivate: [NotAuthenticatedGuard]},
   { path: 'main', loadChildren: () => import('./main-page/main-page.module').then(m => m.MainPageModule), canActivate: [NotAuthenticatedGuard] },
+  { path: 'projection', loadChildren: () => import('./projection/projection.module').then(m => m.ProjectionModule), canActivate: [NotAuthenticatedGuard] },
+  { path: 'movies', loadChildren: () => import('./movies/movies.module').then(m => m.MoviesModule) },
 ];
 
 @NgModule({
