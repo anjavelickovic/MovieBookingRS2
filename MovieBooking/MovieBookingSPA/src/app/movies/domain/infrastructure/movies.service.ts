dbb7007bf93c2f0d397c1bf4d88ee3d6dd11e52a
Немања <nemanja.zivanovic97@hotmail.com>
2022-08-22 16:57:56 +0200

    feat [Angular] Implemented movies facade and infrastructure

--- MovieBooking/MovieBookingSPA/src/app/movies/domain/infrastructure/movies.service.ts
+++ MovieBooking/MovieBookingSPA/src/app/movies/domain/infrastructure/movies.service.ts
@@ -32,4 +32,44 @@ export class MoviesService {
   public GetRandomUpcomingMovies(numberOfMovies: number): Observable<Array<IMovieDetails>> {
     return this.httpClient.get<Array<IMovieDetails>>(`${this.url}/GetRandomUpcomingMovies/${numberOfMovies}`);
   }
+
+  public GetMovieById(id: string): Observable<Array<IMovieDetails>> {
+    return this.httpClient.get<Array<IMovieDetails>>(`${this.url}/${id}`);
+  }
+
+  public GetMoviesByTitle(title: string): Observable<Array<IMovieDetails>> {
+    return this.httpClient.get<Array<IMovieDetails>>(`${this.url}/GetMoviesByTitle/${title}`);
+  }
+
+  public GetMoviesByYear(year: number): Observable<Array<IMovieDetails>> {
+    return this.httpClient.get<Array<IMovieDetails>>(`${this.url}/GetMoviesByYear/${year}`);
+  }
+
+  public GetMoviesByRuntime(lowerBound: number, upperBound: number): Observable<Array<IMovieDetails>> {
+    return this.httpClient.get<Array<IMovieDetails>>(`${this.url}/GetMoviesByRuntime/${lowerBound}/${upperBound}`);
+  }
+
+  public GetMoviesByGenres(genres: string): Observable<Array<IMovieDetails>> {
+    return this.httpClient.get<Array<IMovieDetails>>(`${this.url}/GetMoviesByGenres/${genres}`);
+  }
+
+  public GetMoviesByDirector(director: string): Observable<Array<IMovieDetails>> {
+    return this.httpClient.get<Array<IMovieDetails>>(`${this.url}/GetMoviesByDirector/${director}`);
+  }
+
+  public GetMoviesByMainActor(mainActor: string): Observable<Array<IMovieDetails>> {
+    return this.httpClient.get<Array<IMovieDetails>>(`${this.url}/GetMoviesByMainActor/${mainActor}`);
+  }
+
+  public GetMoviesByLanguage(language: string): Observable<Array<IMovieDetails>> {
+    return this.httpClient.get<Array<IMovieDetails>>(`${this.url}/GetMoviesByLanguage/${language}`);
+  }
+
+  public GetMoviesByImdbRating(lowerBound: number, upperBound: number): Observable<Array<IMovieDetails>> {
+    return this.httpClient.get<Array<IMovieDetails>>(`${this.url}/GetMoviesByImdbRating/${lowerBound}/${upperBound}`);
+  }
+
+  public GetMoviesByImdbVotes(votes: string): Observable<Array<IMovieDetails>> {
+    return this.httpClient.get<Array<IMovieDetails>>(`${this.url}/GetMoviesByImdbVotes/${votes}`);
+  }
 }
