d9db85972fd6fb94ebdd3e681c22dcb8170b2aa5
Немања <nemanja.zivanovic97@hotmail.com>
2022-08-22 23:50:20 +0200

    feat [Angular] Functions for handling user search input implemented

--- MovieBooking/MovieBookingSPA/src/app/movies/feature-search/search/search.component.ts
+++ MovieBooking/MovieBookingSPA/src/app/movies/feature-search/search/search.component.ts
@@ -27,8 +27,138 @@ export class SearchComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    console.log(this.searchCriteria);
-    console.log(this.userSearch);
+
+    switch(this.searchCriteria){
+
+      case "id":
+        this.handleSearchById();
+        return;
+      case "title":
+        this.handleSearchByTitle();
+        return;
+      case "year":
+        this.handleSearchByYear();
+        return;
+      case "runtime":
+        this.handleSearchByRuntime();
+        return;
+      case "genres":
+        this.handleSearchByGenres();
+        return;
+      case "director":
+        this.handleSearchByDirector();
+        return;
+      case "mainActor":
+        this.handleSearchByMainActor();
+        return;
+      case "imdbRating":
+        this.handleSearchByImdbRating();
+        return;
+      case "imdbVotes":
+        this.handleSearchByImdbVotes();
+        return;
+      case "advanced-search":
+        window.alert("advanced search");
+        return;
+      default:
+        window.alert("error");
+        this.router.navigate(['/main']);
+    }
+  }
+
+  private handleSearchById(): void {
+    this.movieService.GetMovieById(this.userSearch).subscribe(
+      (movie: IMovieDetails) => {
+        this.movies = [movie];
+        console.log(this.movies);
+      }
+    )
+  }
+
+  private handleSearchByTitle(): void {
+    this.movieService.GetMoviesByTitle(this.userSearch).subscribe(
+      (movies: Array<IMovieDetails>) => {
+        this.movies = movies;
+        console.log(this.movies);
+      }
+    )
+  }
+
+  private handleSearchByYear(): void {
+    var year = parseInt(this.userSearch);
+    this.movieService.GetMoviesByYear(year).subscribe(
+      (movies: Array<IMovieDetails>) => {
+        this.movies = movies;
+        console.log(this.movies);
+      }
+    )
+  }
+
+  private handleSearchByRuntime(): void {
+    var interval = this.userSearch.split(',');
+    var lowerBound = parseInt(interval[0]);
+    var upperBound =  parseInt(interval[1]);
+    this.movieService.GetMoviesByRuntime(lowerBound, upperBound).subscribe(
+      (movies: Array<IMovieDetails>) => {
+        this.movies = movies;
+        console.log(this.movies);
+      }
+    )
+  }
+
+  // example string: Action&Comedy&Drama/True
+  private handleSearchByGenres(): void {
+    var genres = this.userSearch.split(',');
+    genres = genres.map(genre => genre.trimStart());
+
+    var genresString = genres.join("&");
+
+    this.movieService.GetMoviesByGenres(genresString).subscribe(
+      (movies: Array<IMovieDetails>) => {
+        this.movies = movies;
+        console.log(this.movies);
+      }
+    )
+  }
+
+  private handleSearchByDirector(): void {
+    this.movieService.GetMoviesByDirector(this.userSearch).subscribe(
+      (movies: Array<IMovieDetails>) => {
+        this.movies = movies;
+        console.log(this.movies);
+      }
+    )
+  }
+
+  private handleSearchByMainActor(): void {
+    this.movieService.GetMoviesByMainActor(this.userSearch).subscribe(
+      (movies: Array<IMovieDetails>) => {
+        this.movies = movies;
+        console.log(this.movies);
+      }
+    )
+  }
+
+  private handleSearchByImdbRating(): void {
+    var interval = this.userSearch.split(',');
+    var lowerBound = parseFloat(interval[0]);
+    var upperBound =  parseFloat(interval[1]);
+    this.movieService.GetMoviesByImdbRating(lowerBound, upperBound).subscribe(
+      (movies: Array<IMovieDetails>) => {
+        this.movies = movies;
+        console.log(this.movies);
+      }
+    )
+  }
+
+  private handleSearchByImdbVotes(): void {
+    var votes = parseInt(this.userSearch);
+    this.movieService.GetMoviesByImdbVotes(votes).subscribe(
+      (movies: Array<IMovieDetails>) => {
+        this.movies = movies;
+        console.log(this.movies);
+      }
+    )
   }
 
 }
