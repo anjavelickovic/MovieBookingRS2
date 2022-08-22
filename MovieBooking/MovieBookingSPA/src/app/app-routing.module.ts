42b39306a3a8afd2a9fc9c9582688234c48e9cb1
Немања <nemanja.zivanovic97@hotmail.com>
2022-08-22 23:38:37 +0200

    fix [Angular] All paths bellow ** in the list were redirected to main

--- MovieBooking/MovieBookingSPA/src/app/app-routing.module.ts
+++ MovieBooking/MovieBookingSPA/src/app/app-routing.module.ts
@@ -7,11 +7,11 @@ const routes: Routes = [
   { path: 'identity', loadChildren: () => import('./identity/identity.module').then(m => m.IdentityModule) },
   { path: 'theater-hall', loadChildren: () => import('./theater-hall/theater-hall.module').then(m => m.TheaterHallModule), canActivate: [AdminGuard]},
   { path: 'main', loadChildren: () => import('./main-page/main-page.module').then(m => m.MainPageModule), canActivate: [NotAuthenticatedGuard] },
-  { path: '**', redirectTo: '/main', pathMatch: 'full' },
   { path: 'projection', loadChildren: () => import('./projection/projection.module').then(m => m.ProjectionModule), canActivate: [NotAuthenticatedGuard] },
   { path: 'movies', loadChildren: () => import('./movies/movies.module').then(m => m.MoviesModule), canActivate: [NotAuthenticatedGuard]},
   { path: 'discount', loadChildren: () => import('./discount/discount.module').then(m => m.DiscountModule) , canActivate: [NotAuthenticatedGuard]},
-  { path: 'reservations', loadChildren: () => import('./reservations/reservations.module').then(m => m.ReservationsModule), canActivate: [NotAuthenticatedGuard] }
+  { path: 'reservations', loadChildren: () => import('./reservations/reservations.module').then(m => m.ReservationsModule), canActivate: [NotAuthenticatedGuard] },
+  { path: '**', redirectTo: '/main', pathMatch: 'full' }
 ];
 
 @NgModule({
