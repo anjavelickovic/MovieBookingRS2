b48fa8514539299b2eb7a7297ab5a5110aaa2567
milicas19 <milica7amg@gmail.com>
2022-08-22 16:47:11 +0200

    Modified sort pipe to sort projections by movie title from A-Z

--- MovieBooking/MovieBookingSPA/src/app/projection/pipes/sort-by-movie-name.pipe.ts
+++ MovieBooking/MovieBookingSPA/src/app/projection/pipes/sort-by-movie-name.pipe.ts
@@ -8,7 +8,7 @@ export class SortByMovieNamePipe implements PipeTransform {
 
   transform(projections: IProjection[]): IProjection[] {
     return projections.sort(
-      (proj1, proj2) => 0 - (proj1.movieTitle > proj2.movieTitle ? 1 : -1)
+      (proj1, proj2) => 0 - (proj1.movieTitle < proj2.movieTitle ? 1 : -1)
     );
   }
 
