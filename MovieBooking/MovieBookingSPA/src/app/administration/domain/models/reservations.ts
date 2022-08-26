c05936a7ceb4a3b0e119b8eb1addfda06b3b196a
anjavelickovic <28anja@live.com>
2022-08-26 11:58:19 +0200

    added reservation-list
    getting username needs fixes

--- MovieBooking/MovieBookingSPA/src/app/administration/domain/models/reservations.ts
+++ MovieBooking/MovieBookingSPA/src/app/administration/domain/models/reservations.ts
@@ -2,11 +2,7 @@ import { IReservation } from "./reservation";
 
 export interface IReservations {
     id : number,
-    buyerId : string,
     buyerUserName : string, 
     totalPrice : number,
     tickets : IReservation[]
-
-    areaCode : string,
-    number : string
 }
--- MovieBooking/MovieBookingSPA/src/app/administration/feature-administration/reservations-list/reservations-list.component.ts
+++ MovieBooking/MovieBookingSPA/src/app/administration/feature-administration/reservations-list/reservations-list.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AppState } from 'src/app/shared/app-state/app-state';
+import { AppStateService } from 'src/app/shared/app-state/app-state.service';
+import { AdministrationFacadeService } from '../../domain/application-services/administration-facade.service';
+import { IReservations } from '../../domain/models/reservations';
 
 @Component({
   selector: 'app-reservations-list',
@@ -7,7 +11,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReservationsListComponent implements OnInit {
 
-  constructor() { }
+  public reservations : IReservations[]
+  
+  constructor(private administrationFacadeService : AdministrationFacadeService, 
+    private appStateService: AppStateService) { 
+
+    // dohvatanje username-a
+    appStateService.getAppState().subscribe(appState => {
+      console.log(appState.username);
+      this.administrationFacadeService.getReservationByUsername(appState.username).subscribe(reservations => {
+        this.reservations = reservations;
+      })
+    });
+  }
 
   ngOnInit(): void {
   }
