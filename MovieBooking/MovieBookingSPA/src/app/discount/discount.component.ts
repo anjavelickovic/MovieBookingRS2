aab81fbcfd1804fdb5781a0829a014b2a311f34e
anjavelickovic <28anja@live.com>
2022-08-30 03:07:09 +0200

    Added check for button
    Don't show buttons if there is no coupons in database

--- MovieBooking/MovieBookingSPA/src/app/discount/discount.component.ts
+++ MovieBooking/MovieBookingSPA/src/app/discount/discount.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { DiscountFacadeService } from './domain/application-services/discount-facade.service';
 
 @Component({
   selector: 'app-discount',
@@ -6,10 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./discount.component.css']
 })
 export class DiscountComponent implements OnInit {
+  public couponExists: boolean = false;
+  private activeSubs: Subscription[] = [];
 
-  constructor() { }
+  constructor(private discountFacadeService : DiscountFacadeService) { 
+    var coupons = this.discountFacadeService.getDiscounts().subscribe((coupons) => {
+      if(coupons.length > 0)
+        this.couponExists = true;
+    });
+
+    this.activeSubs.push(coupons);
+  }
 
   ngOnInit(): void {
   }
 
+  ngOnDestroy(){
+    this.activeSubs.forEach((sub: Subscription) => {
+      sub.unsubscribe();
+    })
+  }
 }
